f532b63dbae5d71894f53bc79a447a69f5ae7369
deret <deret@BERKANT>
2023-07-13 16:29:54 +0300

    remember me json formatında

--- Kutuphane_Uygulaması/Form1.cs
+++ Kutuphane_Uygulaması/Form1.cs
@@ -13,9 +13,20 @@ using System.Xml.Serialization;
 using System.IO;
 using System.Configuration;
 using DevExpress.XtraEditors.Repository;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Nodes;
 
 namespace Kutuphane_Uygulaması
 {
+
+    class DbRemember
+    {
+
+        public string KullaniciAdi { get; set; } = string.Empty;
+        public string Sifre { get; set; } = string.Empty;
+        public bool Checkbox { get; set; }
+    }
     public partial class Form1 : Form
     {
         public Form1()
@@ -48,36 +59,40 @@ namespace Kutuphane_Uygulaması
             {
                 string Kadi = textEdit1.Text;
                 string Ksifre = textEdit2.Text;
-                string checkkontrol = checkEdit1.Checked.ToString();
-                string dosyaYolu = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "KullaniciBilgileri.xml");
-                XmlDocument doc = new XmlDocument();
-                
-                doc.Load(dosyaYolu);
-                XmlNodeList aNodes = doc.SelectNodes("/KullaniciBilgileri");
-                doc.SelectNodes("KullaniciBilgileri")[0].SelectSingleNode("KullaniciAdi").InnerText = Kadi;
+                bool checkkontrol = checkEdit1.Checked;
 
-                doc.SelectNodes("KullaniciBilgileri")[0].SelectSingleNode("Sifre").InnerText = Ksifre;
+                string dosyaYolu = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "KullaniciBilgileri.json");
+                var jsonData = new
+                {
+                    KullaniciAdi = Kadi,
+                    Sifre = Ksifre,
+                    Checkbox = checkkontrol
+                };
 
-                doc.SelectNodes("KullaniciBilgileri")[0].SelectSingleNode("checkbox").InnerText = checkkontrol;
-                doc.Save(dosyaYolu);
-                 return true;
+                string jsonString = JsonSerializer.Serialize(jsonData);
+                File.WriteAllText(dosyaYolu, jsonString);
+
+                return true;
             }
-            string dosyaYoluu = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "KullaniciBilgileri.xml");
-            XmlDocument docc = new XmlDocument();
+            else
+            {
+                string dosyaYoluu = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "KullaniciBilgileri.json");
+                var jsonData = new
+                {
+                    KullaniciAdi = "",
+                    Sifre = "",
+                    Checkbox = false
+                };
 
-            docc.Load(dosyaYoluu);
-            XmlNodeList aaNodes = docc.SelectNodes("/KullaniciBilgileri");
-            docc.SelectNodes("KullaniciBilgileri")[0].SelectSingleNode("KullaniciAdi").InnerText = "";
+                string jsonString = JsonSerializer.Serialize(jsonData);
+                File.WriteAllText(dosyaYoluu, jsonString);
 
-            docc.SelectNodes("KullaniciBilgileri")[0].SelectSingleNode("Sifre").InnerText = "";
-
-            docc.SelectNodes("KullaniciBilgileri")[0].SelectSingleNode("checkbox").InnerText = "False";
-            docc.Save(dosyaYoluu);
-
-            return false;
+                return false;
+            }
         }
         private void simpleButton1_Click(object sender, EventArgs e)
         {           
+             
             string Kadi, Ksifre, sifreliKsifre;
             Kadi = textEdit1.Text;
             Ksifre = textEdit2.Text;
@@ -107,35 +122,54 @@ namespace Kutuphane_Uygulaması
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            string dosyaYolu = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "KullaniciBilgileri.xml");
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string json = File.ReadAllText(@"C:\Users\deret\OneDrive\Masaüstü\KullaniciBilgileri.json");
+            var veri = JsonSerializer.Deserialize<DbRemember>(json, options);
 
-            if (File.Exists(dosyaYolu))
-            {
-                XmlDocument xmlDoc = new XmlDocument();
-                xmlDoc.Load(dosyaYolu);
 
-                XmlNode kullaniciAdiNode = xmlDoc.SelectSingleNode("//KullaniciAdi");
-                XmlNode sifreNode = xmlDoc.SelectSingleNode("//Sifre");
-                XmlNode checboxNode = xmlDoc.SelectSingleNode("//checkbox");
 
-                if (kullaniciAdiNode != null && sifreNode != null && checboxNode != null)
-                {
-                    
-                    if (checboxNode.InnerText == "True")
-                    {
-                        checkEdit1.Checked = true;
-                        string kullaniciAdi = kullaniciAdiNode.InnerText;
-                        string sifre = sifreNode.InnerText;
-                        
-                        textEdit1.Text = kullaniciAdi;
-                        textEdit2.Text = sifre;
-                    }
-                    else
-                    {
-                        
-                    }
-                }
-            }
+
+
+            textEdit1.Text = veri.KullaniciAdi;
+            textEdit2.Text = veri.Sifre;
+            checkEdit1.Checked = veri.Checkbox;
+
+
+
+            //string text = File.ReadAllText(@"C:\Users\deret\OneDrive\Masaüstü\KullaniciBilgileri.json");
+            //var remember = JsonSerializer.Deserialize<DbRemember>(text);
+
+            //Console.WriteLine($"First name: {remember.KullaniciAdi}");
+            //Console.WriteLine($"Last name: {remember.Sifre}");
+            //Console.WriteLine($"Job title: {remember.Checkbox}");
+
+            //string dosyaYolu = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "KullaniciBilgileri.json");
+
+            //if (File.Exists(dosyaYolu))
+            //{
+
+            //    string fileName = "WeatherForecast.json";
+            //    string jsonString = File.ReadAllText(fileName);
+            //    jsonData weatherForecast = JsonSerializer.Deserialize<WeatherForecast>(jsonString)!;
+
+
+            //    if (kullaniciAdiNode != null && sifreNode != null && checboxNode != null)
+            //    {
+
+            //        if (checboxNode.InnerText == "True")
+            //        {
+            //            string kullaniciAdi = kullaniciAdiNode.InnerText;
+            //            string sifre = sifreNode.InnerText;
+
+            //            textEdit1.Text = kullaniciAdi;
+            //            textEdit2.Text = sifre;
+            //        }
+            //        else
+            //        {
+
+            //        }
+            //    }
+            //}
         }
     }
 }
--- Kutuphane_Uygulaması/KullaniciGirisForm.cs
+++ Kutuphane_Uygulaması/KullaniciGirisForm.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Windows.Forms;
 
 
-
 namespace Kutuphane_Uygulaması
 {
     public partial class KullaniciGirisForm : Form
